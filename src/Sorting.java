be060e9ba1f2a03ca89c6f314e8040062a4d29ac
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-25 01:16:58 +0200

    bubblesort implementation

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,8 +1,23 @@
 public class Sorting {
     public static void main(String[] args) {
-
+        int array[] = {10, 14, 5, 6, 7, 2, 16, 77, 35, 1};
+        bubbles(array);
+        int i;
+        for (i = 0; i < array.length; i++)
+            System.out.println(array[i]);
     }
 
-    public void bubblesort(int[] array) {
+    private static void bubbles(int[] array) {
+        int i, j, k;
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < array.length - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    k = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = k;
+
+                }
+            }
+        }
     }
 }
