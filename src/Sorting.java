d4b0687cb7c300654ba2d57cd5062259e0dcbc0a
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-25 01:40:14 +0200

    refactoring

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,13 +1,14 @@
 public class Sorting {
     public static void main(String[] args) {
+        Sorting c1 = new Sorting();
         int array[] = {10, 14, 5, 6, 7, 2, 16, 77, 35, 1};
-        bubbles(array);
+        c1.bubbleSort(array);
         int i;
         for (i = 0; i < array.length; i++)
             System.out.println(array[i]);
     }
 
-    private static void bubbles(int[] array) {
+    private void bubbleSort(int[] array) {
         int i, j, k;
         for (i = 0; i < array.length; i++) {
             for (j = 0; j < array.length - i - 1; j++) {
@@ -15,7 +16,6 @@ public class Sorting {
                     k = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = k;
-
                 }
             }
         }
