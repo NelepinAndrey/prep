346086f6f0f94649f80ecf9d6744ba42e0dd10b0
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-25 22:57:43 +0200

    selection sort was implemented correctly

--- src/Sorting.java
+++ src/Sorting.java
@@ -3,41 +3,38 @@ public class Sorting {
         Sorting sorting = new Sorting();
         int array[] = {10, 14, 5, 6, 7, 2, 16, 77, 35, 1};
         sorting.bubbleSort(array);
-        sorting.selection(array);
-        for (int anArray : array) System.out.println(anArray);
+        sorting.selectionSort(array);
+        for (int anArray : array) {
+            System.out.println(anArray);
+        }
     }
 
     private void bubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i] < array[j]) {
-                    swap(array, i, j);
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
                 }
             }
         }
     }
 
-
-    private void swap(int[] array, int i, int j) {
-        int temp;
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
-    private void selection(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int min = i;
+    private void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
             for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[min])
-                    swap2(array, j, min);
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+                swap(array, i, minIndex);
             }
         }
     }
-    private void swap2(int[] array, int j, int min) {
-        int temp;
-        temp = array[j];
-        array[j] = array[min];
-        array[min] = temp;
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
+
