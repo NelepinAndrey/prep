23df8e7f1c6d943c8f7613b2ca0ee4f5c6eae709
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-25 13:11:19 +0200

    refactoring

--- src/Sorting.java
+++ src/Sorting.java
@@ -11,18 +11,18 @@ public class Sorting {
     private void bubbleSort(int[] array) {
         int i, j;
         for (i = 0; i < array.length; i++) {
-            for (j = 0; j < array.length - i - 1; j++) {
-                if (array[j] < array[j + 1]) {
-                    swap(array, j);
+            for (j = 0; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    swap(array, i, j);
                 }
             }
         }
     }
 
-    private void swap(int array[], int j) {
+    private void swap(int[] array, int i, int j) {
         int temp;
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
