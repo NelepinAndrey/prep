9cdeea634f5396eb0ae18cbbe33a1bb58bc89e4b
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-26 23:04:05 +0200

    refactoring

--- src/Recursion.java
+++ src/Recursion.java
@@ -4,9 +4,10 @@ public class Recursion {
         System.out.println(recursion.fib(48));
     }
 
-    private int fib(long n) {
-        if (n == 1 | n == 2)
+    private long fib(long n) {
+        if (n == 1 || n == 2) {
             return 1;
+        }
         return fib(n - 1) + fib(n - 2);
     }
 }
