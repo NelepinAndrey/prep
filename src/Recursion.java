61615409c52d40a1288a2f176f0c1b4716a2b129
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-27 14:57:13 +0200

    add factorial

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,6 +1,7 @@
 public class Recursion {
     public static void main(String[] args) {
         Recursion recursion = new Recursion();
+        System.out.println(recursion.factorial(5));
         System.out.println(recursion.fib(48));
     }
 
@@ -9,5 +10,12 @@ public class Recursion {
             return 1;
         }
         return fib(n - 1) + fib(n - 2);
+    }
+
+    private int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * (factorial(n - 1));
     }
 }
