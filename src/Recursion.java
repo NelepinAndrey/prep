704a00be8ff82c03a0783fa8a72cf73ba5ce645c
NelepinAndrey <andreqnelepin@gmail.com>
2017-12-27 15:09:33 +0200

    refactoring factorial

--- src/Recursion.java
+++ src/Recursion.java
@@ -14,6 +14,9 @@ public class Recursion {
 
     private int factorial(int n) {
         if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
             return 1;
         }
         return n * (factorial(n - 1));
